192b8f59d6e764416f6ee45db5b765b00a9e4419
Yoselin Pascual Mendoza <yoss.pm28@gmail.com>
2026-03-12 19:07:18 -0600

    commit

--- src/Restaurant/App.java
+++ src/Restaurant/App.java
@@ -7,41 +7,31 @@ public class App {
         Mercancia mercancia = new Mercancia(
                 "Carne",
                 50,
-                LocalDate.now(),
+                LocalDate.of(2026,4,4),
                 10
         );
-
         Proveedor proveedor = new Proveedor(
                 "Distribuidora Lopez",
                 "2721234567",
                 "Av Central 45"
         );
-
         Pedido pedido = new Pedido(
                 30,
-                LocalDate.now(),
+                LocalDate.of(2026,4,4),
                 mercancia,
                 proveedor
         );
-
-        pedido.registrarLlegada(30, LocalDate.now());
-
+        pedido.registrarLlegada(30, LocalDate.of(2026,4,11));
         System.out.println("Mercancia:");
         System.out.println(mercancia);
-
         System.out.println("\nProveedor:");
         System.out.println(proveedor);
-
         System.out.println("\nPedido:");
         System.out.println(pedido);
-
         if (mercancia.verificarReorden()) {
             System.out.println("\nSe necesita hacer un nuevo pedido.");
         } else {
-            System.out.println("\nHay suficiente mercancía.");
+            System.out.println("\nHay suficiente mercancía.  :) ");
         }
-
     }
-
-
 }
